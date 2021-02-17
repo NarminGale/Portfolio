67ab12a8ffd80c5c91f7129e43dab5d655802f79
NarminGale <narmingale@gmail.com>
2021-02-17 23:49:43 +0400

    Completed carousel

---
+++ Folio/js/services.js
@@ -0,0 +1,19 @@
+const servicesCarousel = document.querySelector('.services-carousel')
+const serviceBlocks = document.querySelector(
+  '.services-carousel .services-block'
+)
+
+function run() {
+  servicesCarousel.style.transform = `translateX(${-277.5}px)`
+}
+
+setInterval(run, 3000)
+
+servicesCarousel.addEventListener('transitionend', function () {
+  servicesCarousel.appendChild(servicesCarousel.firstElementChild)
+  servicesCarousel.style.transition = 'none'
+  servicesCarousel.style.transform = `translate(0)`
+  setTimeout(function () {
+    servicesCarousel.style.transition = 'transform 0.5s linear'
+  })
+})

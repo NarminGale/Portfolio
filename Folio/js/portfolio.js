bef82447d03af9f0b9e94a670f65151f6d106dad
NarminGale <narmingale@gmail.com>
2021-02-18 18:40:26 +0400

    Completed mouse enter and leave events

---
+++ Folio/js/portfolio.js
@@ -0,0 +1,12 @@
+const portfolioContainer = document.querySelector('.portfolio-container')
+const portfolioItem = document.querySelectorAll('.portfolio-item')
+const portfolioInfo = document.querySelectorAll('.portfolio-info')
+
+portfolioItem.forEach(function (item) {
+  item.addEventListener('mouseenter', function (e) {
+    e.target.children[1].style.display = 'inline-block'
+  })
+  item.addEventListener('mouseleave', function (e) {
+    e.target.children[1].style.display = 'none'
+  })
+})
